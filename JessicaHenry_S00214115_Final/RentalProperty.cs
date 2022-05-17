aa2d4bce3f45c58ba3499af72a06cab3ea821a7e
henry-jessica <jessica.henry@mail.itsligo.ie>
2022-05-17 09:41:45 +0100

    1 - Create a class called RentalProperty 2 - Add a method to increase the rent by a percentage amount

---
+++ JessicaHenry_S00214115_Final/RentalProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JessicaHenry_S00214115_Final
+{
+    public enum RentalType
+    {
+        house, 
+        flat,
+        share
+    }
+    public class RentalProperty
+    {
+        //Propreties 
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public RentalType RentalType { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        //Constructor
+
+        //Method 
+        public decimal IncreaseRentPrice(int percentageAmount)
+        {
+            return Price + (Price * percentageAmount / 100);
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using JessicaHenry_S00214115_Final; 
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange 
+            RentalProperty r1 = new RentalProperty()
+                {
+                    Price = 1000,
+                    Location = "Sligo",
+                };
+
+                //Act
+                decimal expectedResult = 1100;
+                decimal resultOfNewPrice = r1.IncreaseRentPrice(10);
+
+                //Assert 
+                Assert.AreEqual(expectedResult, resultOfNewPrice);
+
+        }
+    }
+}
